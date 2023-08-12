a1881f18149ad508d3cb48bfe2f6851cb2f396ad
samuelobaniwa <treasureobaniwa@gmail.com>
2023-08-12 15:26:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main . A program that print a linewith put function
+ * main . A program that print a line with puts function
  * Return: 0 (success)
  */
 int main(void)
