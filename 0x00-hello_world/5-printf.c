db2b614c64f07fa901868eb44c41e48a1fa3eed1
samuelobaniwa <treasureobaniwa@gmail.com>
2023-08-12 16:21:35 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using the printf function
+ * main - A program that prints a line with  printf function
  * return 0 (success)
+ *
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
 }
+
+
+
 	
 
 	
