ad602285743075781498a22ec9657ef727328fa1
samuelobaniwa <treasureobaniwa@gmail.com>
2023-08-12 16:49:55 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 /**
- * main . A program that prints a line with  printf function
- * Return: 0 (success)
+ * main . A program that print a line using the printf function
+ * Return 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	return (0);
 }
-
-
 
 	
 
