dd84412a1e9b1c6deb109fc47c96af72a2f68f0c
samuelobaniwa <treasureobaniwa@gmail.com>
2023-08-14 06:01:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,7 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdin);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,stdin);
 	return (1);
 }
-
 
