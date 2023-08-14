f3be175652a5661a2925acdfa4304f70c5253a29
samuelobaniwa <treasureobaniwa@gmail.com>
2023-08-14 04:29:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-       	what(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	return (1)
 }
 
 
