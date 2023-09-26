825ec6c5e11d16beb2076b5c8bdef79cfd8d5847
samuelobaniwa <treasureobaniwa@gmail.com>
2023-09-25 17:04:49 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,7 +3,7 @@
 /**
  * add_nodeint_end - adds a node at the end of a linked list
  * @head: pointer to the first element in the list
- * @m: data to insert in the new element
+ * @n: data to insert in the new element
  *
  * Return: pointer to the new node, or NULL if it fails
  */
@@ -16,7 +16,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (!new)
 		return (NULL);
 
-	new->m = m;
+	new->n = n;
 	new->next = NULL;
 
 	if (*head == NULL)
