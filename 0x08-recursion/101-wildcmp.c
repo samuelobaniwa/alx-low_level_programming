c6f8a58e7ed709b52d30f37de6df47ece3576f70
samuelobaniwa <treasureobaniwa@gmail.com>
2023-10-26 02:11:13 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,14 +11,14 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
-		if (s2 != '\0' && *s2 == '')
+		if (*s2 != '\0' && *s2 == '*')
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
 		return (*s2 == '\0');
 	}
 
-	if (s2 == '')
+	if (*s2 == '*')
 	{
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
@@ -28,3 +28,5 @@ int wildcmp(char *s1, char *s2)
 	}
 	return (0);
 }
+
+
