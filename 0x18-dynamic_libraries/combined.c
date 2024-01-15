1c69c1972f45d22adc148d3f5437397fd43b22ec
samuelobaniwa <treasureobaniwa@gmail.com>
2024-01-15 13:17:44 -0800

    commit using library in python that contain c function

---
+++ 0x18-dynamic_libraries/combined.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * mod - modulle two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: modullus
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * div - divide two integers
+ * @a: first integers
+ * @b: second integer
+ * Return: division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
